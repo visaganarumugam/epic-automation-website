55eea579a58c6eb330a724e2e4129a821b67e99f
mohammedsharjun <dhanishsharjun9@gmail.com>
2025-06-26 17:08:06 +0530

    chnaged files

--- client/src/components/ui_components/scroll-based-velocity.tsx
+++ client/src/components/ui_components/scroll-based-velocity.tsx
@@ -72,7 +72,7 @@ function ParallaxText({
   const x = useTransform(baseX, (v) => `${wrap(-100 / repetitions, 0, v)}%`);
 
   const directionFactor = React.useRef<number>(1);
-  useAnimationFrame((t, delta) => {
+  useAnimationFrame((_, delta) => {
     let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
 
     if (velocityFactor.get() < 0) {
